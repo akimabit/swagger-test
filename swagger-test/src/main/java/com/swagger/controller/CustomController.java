6395989204d2a6fc2d8598d1ab7641bf5166a800
金孝錫 <hyosuk.kim@nhn-japan.com>
2018-04-03 15:54:38 +0900

    commit

--- swagger-test/src/main/java/com/swagger/controller/CustomController.java
+++ swagger-test/src/main/java/com/swagger/controller/CustomController.java
@@ -1,15 +1,28 @@
 package com.swagger.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.swagger.model.ProductDto;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
+@Api(value = "Online Customer", description = "Online Customer Center in Tokyo.")
+@RequestMapping("/v1")
 public class CustomController {
 
-	@RequestMapping(value = "/custom", method = RequestMethod.POST)
-	public String custom() {
-		return "custom";
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Successfully retrieved list"),
+			@ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+			@ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+			@ApiResponse(code = 404, message = "Pathが見つかりません。") })
+	@PostMapping(value = "/custom/{id}", produces = "application/json")
+	public ProductDto custom(@RequestBody ProductDto dto) {
+		return dto;
 	}
 
 }
